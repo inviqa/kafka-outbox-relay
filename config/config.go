c916f9b5de864fbac65c5a417381dcd70a7038a8
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2021-07-02 16:23:23 +0100

    Fix bug in postgres credentials escaping with special characters
    URLs need each component url escaping. Particularly where their components can support characters that split the components up (such as ?, @ and : ).
    
    This is most affected with some password generators when special characters are used.
    
    The MySQL driver seems unaffected by it, and breaks if done correctly

--- config/config.go
+++ config/config.go
@@ -85,7 +85,7 @@ func (c *Config) GetDSN() string {
 				sslMode = "verify-full"
 			}
 		}
-		return fmt.Sprintf("%s://%s:%s@%s:%d/%s?sslmode=%s", c.DBDriver, c.DBUser, c.DBPass, c.DBHost, c.DBPort, c.DBSchema, sslMode)
+		return fmt.Sprintf("%s://%s@%s:%d/%s?sslmode=%s", c.DBDriver, url.UserPassword(c.DBUser, c.DBPass), c.DBHost, c.DBPort, c.DBSchema, sslMode)
 	default:
 		log.Logger.Fatalf("the DB driver configured (%s) is not supported", c.DBDriver)
 		return ""
