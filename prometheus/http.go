abd2ab49b9a198d8de67d5b4c579db6dd1486f39
James Halsall <james.halsall@inviqa.com>
2021-10-08 09:54:50 +0100

    Gracefully stop HTTP monitoring server (#29)

--- prometheus/http.go
+++ prometheus/http.go
@@ -1,20 +1,40 @@
 package prometheus
 
 import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
 	"inviqa/kafka-outbox-relay/config"
 	h "inviqa/kafka-outbox-relay/http"
 	"inviqa/kafka-outbox-relay/log"
-	"net/http"
-
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func StartHttpServer(cfg *config.Config, db h.Pinger) {
-	http.Handle("/metrics", promhttp.Handler())
-	http.Handle("/healthz", h.NewHealthzHandler(cfg.GetDependencySystemAddresses(), db))
+func StartHttpServer(ctx context.Context, cfg *config.Config, db h.Pinger) {
+	var srv http.Server
 
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-		log.Logger.Fatalf("failed to start prometheus HTTP server: %s", err)
+	go func() {
+		select {
+		case _ = <-ctx.Done():
+			httpCtx, httpCancel := context.WithTimeout(context.Background(), 3*time.Second)
+			defer httpCancel()
+			if err := srv.Shutdown(httpCtx); err != nil {
+				log.Logger.WithError(err).Error("HTTP server did not shutdown correctly")
+			}
+		}
+	}()
+
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.Handle("/healthz", h.NewHealthzHandler(cfg.GetDependencySystemAddresses(), db))
+	srv = http.Server{
+		Handler: mux,
+		Addr:    ":80",
+	}
+
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Logger.Fatalf("failed to start HTTP server: %s", err)
 	}
 }
