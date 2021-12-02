f2b8dee53d3cc47b9afbd2a325b7494e0ed91453
James Halsall <james.halsall@inviqa.com>
2021-12-02 11:57:58 +0000

    Fix benchmarks

--- benchmarks/bench_test.go
+++ benchmarks/bench_test.go
@@ -4,7 +4,6 @@
 package benchmarks
 
 import (
-	"database/sql"
 	"fmt"
 
 	benchkafka "inviqa/kafka-outbox-relay/benchmarks/kafka"
@@ -17,7 +16,7 @@ import (
 var (
 	repo         outbox.Repository
 	cfg          *config.Config
-	db           *sql.DB
+	db           data.DB
 	pub          kafka.Publisher
 	syncProducer *benchkafka.SyncProducer
 )
@@ -25,36 +24,38 @@ var (
 func init() {
 	cfg = createConfig()
 
-	db = data.NewDBs(cfg)
+	dbs, _ := data.NewDBs(cfg)
+	db = dbs[0]
+
 	ensureOutboxTableExists()
 
-	repo = outbox.NewRepository(db, cfg)
+	repo = outbox.NewRepository(dbs[0], cfg)
 	syncProducer = benchkafka.NewSyncProducer(cfg.KafkaHost)
 	pub = kafka.NewPublisherWithProducer(syncProducer)
 }
 
 func purgeOutboxTable() {
-	_, err := db.Exec(fmt.Sprintf("TRUNCATE TABLE `%s`;", cfg.DBOutboxTable))
+	_, err := db.Connection().Exec(fmt.Sprintf("TRUNCATE TABLE `%s`;", db.Config().OutboxTable))
 	if err != nil {
 		panic(fmt.Sprintf("an error occurred cleaning the outbox table for tests: %s", err))
 	}
 }
 
 func ensureOutboxTableExists() {
-	_, err := db.Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s LIKE kafka_outbox;", cfg.DBOutboxTable))
+	_, err := db.Connection().Exec(fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s LIKE kafka_outbox;", db.Config().OutboxTable))
 	if err != nil {
 		panic(fmt.Sprintf("an error occurred creating the outbox table for integration tests: %s", err))
 	}
 }
 
 func insertOutboxMessages(msgs []*outbox.Message) {
-	tx, err := db.Begin()
+	tx, err := db.Connection().Begin()
 	if err != nil {
 		panic(fmt.Sprintf("error creating a DB transaction: %s", err))
 	}
 
 	for _, msg := range msgs {
-		q := fmt.Sprintf("INSERT INTO `%s` SET topic = ?, payload_json = ?, payload_headers = '{}';", cfg.DBOutboxTable)
+		q := fmt.Sprintf("INSERT INTO `%s` SET topic = ?, payload_json = ?, payload_headers = '{}';", db.Config().OutboxTable)
 
 		_, err = tx.Exec(q, msg.Topic, msg.PayloadJson)
 		if err != nil {
@@ -70,13 +71,17 @@ func insertOutboxMessages(msgs []*outbox.Message) {
 
 func createConfig() *config.Config {
 	cfg = &config.Config{
-		DBHost:          "localhost",
-		DBPort:          13306,
-		DBUser:          "kafka-outbox-relay",
-		DBPass:          "kafka-outbox-relay",
-		DBName:          "kafka-outbox-relay",
-		DBDriver:        config.MySQL,
-		DBOutboxTable:   "kafka_outbox_test",
+		DBs: []config.Database{
+			{
+				Host:        "localhost",
+				Port:        13306,
+				User:        "kafka-outbox-relay",
+				Password:    "kafka-outbox-relay",
+				Name:        "kafka-outbox-relay",
+				Driver:      config.MySQL,
+				OutboxTable: "kafka_outbox_test",
+			},
+		},
 		KafkaHost:       []string{"localhost:9092"},
 		PollFrequencyMs: 500,
 	}
